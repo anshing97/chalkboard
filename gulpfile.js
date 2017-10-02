a3de64419e250b1e1d409aac1dcef88d71d10fff
Ben Chao <ben@informedk12.com>
2017-10-01 15:58:15 -1000

    some stuff

--- gulpfile.js
+++ gulpfile.js
@@ -6,63 +6,64 @@ var gulp        = require('gulp'),
     sequence    = require('run-sequence');
 
 gulp.task('clean', function () {
-    return gulp.src('public', {read: false})
-        .pipe(clean());
+  return gulp.src('public', {read: false})
+    .pipe(clean());
 });
 
 gulp.task('sass', function() {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass({
-            includePaths: 'node_modules/bootstrap/',
-        }))
-        .pipe(gulp.dest('public/css'));
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass({
+      includePaths: 'node_modules/bootstrap/',
+    }))
+    .pipe(gulp.dest('public/css'));
 });
 
 
 gulp.task('styleguide', function() {
 
-    var options = {
-        source: [
-            'src/scss/',
-        ],
-        destination: 'public/styelguide/',
+  var options = {
+    source: [
+      'src/scss/',
+    ],
+    destination: 'public/styelguide/',
 
-        // builder
-        builder: 'src/styleguide/builder/',
+    // just a custome builder
+    builder: 'src/styleguide/builder/',
 
-        // The css and js paths are URLs, like '/misc/jquery.js'.
-        // The following paths are relative to the generated style guide.
-        css: [
-            '../css/chalkboard.css'
-        ],
-        js: [
-        ],
+    // The css and js paths are URLs, like '/misc/jquery.js'.
+    // The following paths are relative to the generated style guide.
+    css: [
+      '../css/chalkboard.css'
+    ],
+    js: [
+    ],
 
-        homepage: 'styleguide/home.md',
-        title: 'Chalkboard'
-    };
+    homepage: 'styleguide/home.md',
+    title: 'Chalkboard'
+  };
 
-    return kss(options);
+  return kss(options);
 });
 
 gulp.task('connect', function() {
-    connect.server({
-        root: 'public',
-        livereload: true
-    });
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/scss/**/*.scss', ['sass','styleguide']);
+  gulp.watch('src/scss/**/*.scss', ['sass','styleguide']);
+  gulp.watch('src/styleguide/**/*.hbs',['styleguide']);
 });
 
 
 
 
 gulp.task('default', function() {
-    sequence('clean',
-             'sass',
-             'styleguide',
-             'connect',
-             'watch');
+  sequence('clean',
+           'sass',
+           'styleguide',
+           'connect',
+           'watch');
 });
