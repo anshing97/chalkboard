fcc17f8490f296330ebf3b5d7ac08cda146be6d0
Ben Chao <bchao@cs.washington.edu>
2017-10-14 03:03:17 -0700

    got the page

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,12 @@ var gulp            = require('gulp'),
     data            = require('gulp-data'),
     jsonFormat      = require('gulp-json-format');
     livingcss       = require('gulp-livingcss'),
+    mergeJSON       = require('gulp-merge-json'),
     rename          = require('gulp-rename'),
-    sass            = require('gulp-sass'),
     nunjucksRender  = require('gulp-nunjucks-render'),
+    sass            = require('gulp-sass'),
     sequence        = require('run-sequence'),
+    importFresh     = require('import-fresh'),
     readFiles       = require('read-vinyl-file-stream');
     path            = require('path'),
     fs              = require('fs');
@@ -33,29 +35,33 @@ gulp.task('sass', function() {
 gulp.task('styleguide', function () {
   return gulp.src('src/scss/**/*.scss')
     .pipe(livingcss('',options.livingcss))
+    .pipe(mergeJSON(options.mergeJSON))
     .pipe(jsonFormat(2))
     .pipe(gulp.dest('public/styleguide/data'));
 });
 
 gulp.task('nunjucks', function () {
-  return gulp.src('public/styleguide/data/*.json')
-    .pipe(foreach(function(stream, file) {
 
-      var json_file = path.relative(file.cwd, file.path);
-      var json_data = fs.readFileSync(json_file,'utf8');
+  let cssData = importFresh('./public/styleguide/data/css.json');
 
-      console.log("processing file " + json_file);
-
-
+  return gulp.src('src/styleguide/templates/layout.html')
+    .pipe(foreach(function(stream, file){
       console.log(file.relative);
+      console.log('process one');
+      // loop through all the json array, each one represents a unique scenario
+      // so we want a new file for each
 
-      stream.pipe(clone())
-        .pipe(data({
-            testing: 'stuff'
-        }))
-        .pipe(nunjucksRender(options.nunjucks))
-        .pipe(rename(file.relative + '.html'))
-        .pipe(gulp.dest('public/styleguide/'));
+      for ( var ii = 0; ii < cssData.allSections.length; ii++) {
+
+        let this_data = cssData.allSections[ii];
+        console.log(this_data);
+
+        stream.pipe(clone())
+          .pipe(data(this_data))
+          .pipe(nunjucksRender(options.nunjucks))
+          .pipe(rename(this_data.id + '.html'))
+          .pipe(gulp.dest('public/styleguide'));
+      }
 
       return stream;
     }));
--- src/styleguide/options.js
+++ src/styleguide/options.js
@@ -6,13 +6,50 @@ const livingcss = {
 };
 
 const nunjucks = {
-  path: 'src/styleguide/templates/',
+  path: 'src/styleguide/templates/partials/',
   envOptions: {
     autoescape: false
   }
 };
 
-module.exports = { livingcss, nunjucks }
+const mergeJSON = {
+  fileName: 'css.json',
+  edit: (parsedJson, file) => {
+      if (parsedJson.pageOrder) {
+          delete parsedJson.pageOrder;
+      }
+      if (parsedJson.stylesheets) {
+          delete parsedJson.stylesheets;
+      }
+      if (parsedJson.footerHTML) {
+          delete parsedJson.footerHTML;
+      }
+      if (parsedJson.id) {
+          delete parsedJson.id;
+      }
+      if (parsedJson.globalStylesheets) {
+          delete parsedJson.globalStylesheets;
+      }
+      if (parsedJson.menuButtonHTML) {
+          delete parsedJson.menuButtonHTML;
+      }
+      if (parsedJson.scripts) {
+          delete parsedJson.scripts;
+      }
+      if (parsedJson.sections) {
+          delete parsedJson.sections;
+      }
+      if (parsedJson.title) {
+          delete parsedJson.title;
+      }
+      if (parsedJson.pages) {
+          delete parsedJson.pages;
+      }
+      return parsedJson;
+  },
+};
+
+module.exports = { livingcss, nunjucks, mergeJSON }
 
 
 
